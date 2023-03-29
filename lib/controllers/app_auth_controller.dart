ba82501eb7c54597e4d508962069f2ab01aa89fd
Plekhanov <32983332+VekTor2300@users.noreply.github.com>
2023-03-30 00:31:10 +0300

    api docker

---
+++ lib/controllers/app_auth_controller.dart
@@ -0,0 +1,154 @@
+import 'dart:io';
+import 'package:conduit/conduit.dart';
+import 'package:api/model/response.dart';
+import 'package:api/utils/app_utils.dart';
+import 'package:api/model/user.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
+
+class AppAuthContoler extends ResourceController {
+  AppAuthContoler(this.managedContext);
+
+  final ManagedContext managedContext;
+
+  @Operation.post()
+  Future<Response> signIn(@Bind.body() User user) async {
+    if (user.password == null || user.userName == null) {
+      return Response.badRequest(
+          body: ModelResponse(message: 'Поля  password username обязательны'));
+    }
+
+    try {
+      final qFindUser = Query<User>(managedContext)
+        ..where((element) => element.userName).equalTo(user.userName)
+        ..returningProperties(
+          (element) => [
+            element.id,
+            element.salt,
+            element.hashPassword,
+          ],
+        );
+
+      final findUser = await qFindUser.fetchOne();
+      if (findUser == null) {
+        throw QueryException.input('Пользователь не найден', []);
+      }
+      final requestHashPassword =
+          generatePasswordHash(user.password ?? '', findUser.salt ?? '');
+
+      if (requestHashPassword == findUser.hashPassword) {
+        _updateTokens(findUser.id ?? -1, managedContext);
+
+        final newUser =
+            await managedContext.fetchObjectWithID<User>(findUser.id);
+
+        return Response.ok(ModelResponse(
+          data: newUser!.backing.contents,
+          message: 'Успешная авторизация',
+        ));
+      } else {
+        throw QueryException.input('Не верный пароль', []);
+      }
+    } on QueryException catch (e) {
+      return Response.serverError(body: ModelResponse(message: e.message));
+    }
+  }
+
+  @Operation.put()
+  Future<Response> signUp(@Bind.body() User user) async {
+    if (user.password == null || user.userName == null || user.email == null) {
+      return Response.badRequest(
+        body:
+            ModelResponse(message: 'Поля  password username email обязательны'),
+      );
+    }
+
+    // Генерация соли
+    final salt = generateRandomSalt();
+    // Генерация хэша пароля
+    final hashPassword = generatePasswordHash(user.password!, salt);
+
+    try {
+      late final int id;
+      // Создаем транзакицю
+      await managedContext.transaction((transaction) async {
+        // Создаем запрос на создания пользователя
+        final qCreateUser = Query<User>(transaction)
+          ..values.userName = user.userName
+          ..values.email = user.email
+          ..values.salt = salt
+          ..values.hashPassword = hashPassword;
+        // Добавление пользоваетля в бд
+        final createdUser = await qCreateUser.insert();
+        // Сохранение id пользователя
+        id = createdUser.id!;
+        // Обновление токена
+        _updateTokens(id, transaction);
+      });
+
+      // Получаем данные пользователя по сохраненному id
+      final userData = await managedContext.fetchObjectWithID<User>(id);
+
+      return Response.ok(
+        ModelResponse(
+          data: userData!.backing.contents,
+          message: 'Пользователь успешно зарегистрировался',
+        ),
+      );
+    } on QueryException catch (e) {
+      return Response.serverError(body: ModelResponse(message: e.message));
+    }
+  }
+
+  @Operation.post('refresh')
+  Future<Response> refreshToken(
+      @Bind.path('refresh') String refreshToken) async {
+    try {
+      final id = AppUtils.getIdFromToken(refreshToken);
+
+      final user = await managedContext.fetchObjectWithID<User>(id);
+
+      if (user!.refreshToken != refreshToken) {
+        return Response.unauthorized(body: 'Token не валидный');
+      }
+
+      _updateTokens(id, managedContext);
+
+      return Response.ok(
+        ModelResponse(
+          data: user.backing.contents,
+          message: 'Токен успешно обновлен',
+        ),
+      );
+    } on QueryException catch (e) {
+      return Response.serverError(body: ModelResponse(message: e.message));
+    }
+  }
+
+  void _updateTokens(int id, ManagedContext transaction) async {
+    final Map<String, String> tokens = _getTokens(id);
+
+    final qUpdateTokens = Query<User>(transaction)
+      ..where((element) => element.id).equalTo(id)
+      ..values.accessToken = tokens['access']
+      ..values.refreshToken = tokens['refresh'];
+
+    await qUpdateTokens.updateOne();
+  }
+
+  //Генерация jwt token
+  Map<String, String> _getTokens(int id) {
+    final key = Platform.environment['SECRET_KEY'] ?? 'SECRET_KEY';
+    final accessClaimSet = JwtClaim(
+      maxAge: const Duration(hours: 1), // Время жизни token
+      otherClaims: {'id': id},
+    );
+    final refreshClaimSet = JwtClaim(
+      otherClaims: {'id': id},
+    );
+    final tokens = <String, String>{};
+    tokens['access'] = issueJwtHS256(accessClaimSet, key);
+    tokens['refresh'] = issueJwtHS256(refreshClaimSet, key);
+
+    return tokens;
+  }
+}
